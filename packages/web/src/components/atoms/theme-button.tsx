1756a9cfc83ec7b11746f3543fa05eea1d56bf53
Fedeya <elfedeminaya@gmail.com>
2020-09-16 18:56:26 -0300

    migration: migrated to chakra v1

--- packages/web/src/components/atoms/theme-button.tsx
+++ packages/web/src/components/atoms/theme-button.tsx
@@ -1,15 +1,22 @@
-import { useColorMode, IconButton, IconButtonProps } from '@chakra-ui/core';
+import {
+  useColorMode,
+  IconButton,
+  IconButtonProps,
+  useColorModeValue
+} from '@chakra-ui/core';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 const ThemeButton: React.FC<Omit<IconButtonProps, 'aria-label'>> = props => {
-  const { toggleColorMode, colorMode } = useColorMode();
+  const { toggleColorMode } = useColorMode();
+  const icon = useColorModeValue(<FaMoon />, <FaSun />);
 
   return (
     <IconButton
       aria-label="change color theme"
-      icon={colorMode === 'light' ? 'moon' : 'sun'}
+      icon={icon}
       onClick={toggleColorMode}
       variant="ghost"
-      variantColor="purple"
+      colorScheme="purple"
       {...props}
     />
   );
